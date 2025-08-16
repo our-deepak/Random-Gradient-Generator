ad3f19fadc395ef622e7aa17fee16b5096ba5a9e
Deepak Kumar Gupta <141762886+our-deepak@users.noreply.github.com>
2025-08-16 12:26:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let hexarr = "0123456789ABCDEF";
+let code1="#123456";
+let code2="#ABCDEF";
+let text= document.querySelector('.colorcode');
+const changecode1 = ()=>{
+    code1="#";
+    for(let i=0;i<6;i++) code1+= hexarr[Math.floor(16*Math.random())];
+};
+const changecode2 = ()=>{
+    code2="#";
+    for(let i=0;i<6;i++) code2+= hexarr[Math.floor(16*Math.random())];
+    console.log(code2);
+};
+const applyGradient = ()=>{
+    document.body.style.backgroundImage=`linear-gradient(to right, ${code1}, ${code2})`;
+    text.textContent = `background-image: linear-gradient(to right, ${code1}, ${code2});`;
+    document.getElementById('btn1').textContent= code1;
+    document.getElementById('btn2').textContent= code2;
+}
+document.getElementById('btn1').addEventListener("click",()=>{
+    changecode1();
+    applyGradient();
+});
+document.getElementById('btn2').addEventListener("click",()=>{
+    changecode2();
+    applyGradient();
+});
